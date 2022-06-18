2497650f720a1184854be4cf687cfe06d44b6254
Chaosick <patrykwiecek5@wp.pl>
2022-06-18 15:35:41 +0200

    Poprawki
    Connection to DB is working;
    Problem: 'Float' not 'float' in import and usage

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,22 @@
 from fastapi import FastAPI
-from typing import Optional
-import schemas, models
-from backend.database import engine
+from . import models, schemas
+from .database import engine
 
 app = FastAPI()
 models.Base.metadata.create_all(engine)
 
 @app.get('/')
-def index():
+async def index():
     return{'data':'Herro my names Rajesh'}
 
 @app.get('/items')
-def allItems():
+async def allItems():
     return{'data':{'Such items'}}
 
 @app.get('/items/{cat_id}')
-def catalog(cat_id:int):
+async def catalog(cat_id:int):
     return{'data':{cat_id}}
 
 @app.get("/items/{cat_id}/{id}")
-def getItem(cat_id:int , id):
+async def getItem(cat_id:int , id):
     return{'data':{cat_id:{id}}}
--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,12 @@
 from .database import Base
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship
 
 
-class product(Base):
-    __tablename__ = 'Product'
-    p_id = Column(Integer, primary_key=True, index=True)
+class Product(Base):
+    __tablename__ = "product"
+    p_id = Column(Integer, primary_key=True)
     name = Column(String)
     atributes = Column(String)
-    price = Column(float)
+    price = Column(Float)
 
